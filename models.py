262515f11da613fb7afaf3c781d3b86a4772978b
Nay <nay.fullstack@gmail.com>
2025-04-14 11:25:33 +0200

    agregada segunda tabla

--- models.py
+++ models.py
@@ -22,3 +22,25 @@ class Movie(db.Model):
 
     def __repr__(self):
         return f'<Movie {self.title}>'
+
+#creamos una segunda tabla relacionada directamente con la primera con la foreignkey y movies.id
+class Loan(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    movie_id = db.Column(db.Integer, db.ForeignKey('movie.id'), nullable=False)
+    borrower = db.Column(db.String(100), nullable=False)
+    loan_date = db.Column(db.Date, nullable=False)
+    return_date = db.Column(db.Date, nullable=True)
+    returned = db.Column(db.Boolean, default=False)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'movie_id': self.movie_id,
+            'borrower': self.borrower,
+            'loan_date': self.loan_date.isoformat(),
+            'return_date': self.return_date.isoformat() if self.return_date else None,
+            'returned': self.returned,
+        }
+
+    def __repr__(self):
+        return f'<Loan {self.borrower} - {self.movie.title}>'
